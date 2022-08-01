13d6308da912c7defef4f35ca9f7f2e8727dfa9b
Ethan <ethanbaird94@gmail.com>
2022-08-01 13:36:04 +0100

    added task list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState(['milk cow', 'make cheese', 'eat cheese']) 
+
+  const tasksNodes = tasks.map((task, index) => {
+    return (
+      <li>
+        {task}
+      </li>
+    )
+  });
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do's</h1>
+      
+      <form>
+        <label htmlFor='add-task'>Add Task:</label>
+        <input id='new-task' type='text'/>
+      </form>
+
+      <ul>
+        {tasksNodes}
+      </ul>
     </div>
   );
 }
