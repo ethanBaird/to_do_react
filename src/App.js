a29e7a30faf9bb1dda8f94828ad43b21714a5cef
Ethan <ethanbaird94@gmail.com>
2022-08-01 20:36:06 +0100

    worked on 'there are no tasks', still to fix

--- src/App.js
+++ src/App.js
@@ -4,63 +4,88 @@ import { useState } from 'react';
 
 function App() {
   const [tasks, setTasks] = useState([
-    {name: 'milk cow', priority: 'low'},
-    {name: 'make cheese', priority: 'high'},
-    {name: 'eat cheese', priority: 'high'}
+    {name: 'milk cow', priority: 'low', completed: false},
+    {name: 'make cheese', priority: 'high', completed:true},
+    {name: 'eat cheese', priority: 'high', completed: false}
     ]) 
 
-  const [newTaskName, setNewTaskName] = useState('');
-  const [newTaskPriority, setNewTaskPriority] = useState('')
+  const [newTask, setNewTask] = useState('');
 
-  const tasksNodes = tasks.map((task, index) => {
-    return (
-      <li key={index} className= {task.priority}>
-        {task.name}
-      </li>
-    )
+  const tasksNodes = tasks.map((task, index, tasks) => {
+    if (task.completed === false && tasks.length !== 0) {
+      return (
+        <li key={index} className= {task.priority}>
+          <h2>{task.name}</h2>
+          <button className='button' onClick={() => {handleTaskComlpeted(index)}}>Completed</button>
+        </li>
+      )
+    }
+    else if (tasks.every( (task) => task.completed === true)){
+      return (
+        <li key={index}>
+          <h2>There are no tasks to do!</h2>
+        </li>
+      )
+    }
   });
 
-  const handleTaskNameInput = (event) => {
-    setNewTaskName(event.target.value)
+  const completedTasksNodes = tasks.map((task, index) => {
+    if (task.completed === true) {
+      return (
+      <li key={index} className='completed'>
+        <h2>{task.name}</h2>
+      </li>
+      )
+    };
+  });
+
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value)
   }
 
-  const handleTaskPriorityInput = (event) => {
-    setNewTaskPriority(event.target.value)
+  const handleTaskComlpeted = (index) => {
+    const copyTasks = [...tasks];
+    copyTasks[index].completed = true;
+    setTasks(copyTasks);
   }
-
 
   const saveNewTask = (event) => {
     event.preventDefault();
     const copyTasks = [...tasks];
     copyTasks.push({
-      name: newTaskName,
-      priority: newTaskPriority
+      name: newTask,
+      priority: event.target.priority.value,
+      completed: false
     })
     setTasks(copyTasks);
-    setNewTaskName('');
+    setNewTask('');
   }
 
   
   
   return (
     <div className="App">
-      <h1>To Do's</h1>
+      <h1>To Do List</h1>
       
       <form onSubmit={saveNewTask}>
         <div className='task-name'>
           <label htmlFor='add-task'>Add Task:</label>
-          <input id='add-task' type='text' value={newTaskName} onChange={handleTaskNameInput}/>
+          <input id='add-task' type='text' name='name' value={newTask} onChange={handleTaskInput}/>
         </div>
         <div className='radio'>
           <label htmlFor='priority'>Priority:</label>
-          <input className='radio-button' id='priority' type='radio' name='priority' value='high' onChange={handleTaskPriorityInput} /><span>high</span>
-          <input className='radio-button' id='priority' type='radio' name='priority' value='low' onChange={handleTaskPriorityInput}/><span>low</span>
+          <input className='radio-button' id='priority' type='radio' name='priority' value='high'/><span>high</span>
+          <input className='radio-button' id='priority' type='radio' name='priority' value='low'/><span>low</span>
         </div>
         <input className='button' type='submit' value='save task'/>
       </form>
-
+      <h2>To Do</h2>
       <ul>
         {tasksNodes}
+      </ul>
+      <h2>Done</h2>
+      <ul>
+        {completedTasksNodes}
       </ul>
     </div>
   );
